842fa6e63844eccf4b3a89830cb3fb5f2484b0bb
Tom Vejmelka <vejtom@gmail.com>
2024-09-27 17:58:04 +0200

    feat: add main layout

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from '../components/general/Header';
+import { Footer } from '../components/general/Footer';
+
+const MainLayout = () => {
+  return (
+    <div className="bg-gray-100">
+      <div className="flex min-h-screen flex-col w-full max-w-7xl mx-auto">
+        <Header />
+        <main className="flex flex-grow justify-center py-5 md:py-0">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
