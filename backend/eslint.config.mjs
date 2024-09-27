58aa45cbd08b346de9aa3a00f57452bd4fef2eba
Tom Vejmelka <vejtom@gmail.com>
2024-09-27 17:49:58 +0200

    chore: add more rules to eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  {ignores: ["dist/"]},
+  {ignores: ['dist/']},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {rules: {
+    'semi': ['error', 'always'],
+    'no-trailing-spaces': ['error'],
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+  }}
 ];
